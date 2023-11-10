d67537d2a092e035dede7203159627eff6881f58
VinGitonga <gitongavincent64@gmail.com>
2023-11-10 09:19:49 +0300

    filled products page

--- client/src/pages/manufacturer/my-products.tsx
+++ client/src/pages/manufacturer/my-products.tsx
@@ -1,8 +1,224 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import ManufacturerLayout from "@/layouts/ManufacturerLayout";
 import { NextPageWithLayout } from "@/types/Layout";
+import { ColumnDef, ColumnFiltersState, SortingState, VisibilityState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
+import { ChevronDown, MoreHorizontal } from "lucide-react";
+import { useState } from "react";
+
+interface TMyProduct {
+	id: number;
+	name: string;
+	code: string;
+	quantity: number;
+	rawMaterials: string[];
+	batchNo: string;
+}
+
+const myProducts: TMyProduct[] = [
+	{
+		id: 1,
+		name: "Product 1",
+		code: "12345678",
+		quantity: 100,
+		rawMaterials: ["12345678", "23456789", "34567890"],
+		batchNo: "12345678",
+	},
+	{
+		id: 2,
+		name: "Product 2",
+		code: "23456789",
+		quantity: 200,
+		rawMaterials: ["23456789", "34567890", "45678901"],
+		batchNo: "23456789",
+	},
+	{
+		id: 3,
+		name: "Product 3",
+		code: "34567890",
+		quantity: 300,
+		rawMaterials: ["34567890", "45678901", "56789012"],
+		batchNo: "34567890",
+	},
+	{
+		id: 4,
+		name: "Product 4",
+		code: "45678901",
+		quantity: 400,
+		rawMaterials: ["45678901", "56789012", "67890123"],
+		batchNo: "45678901",
+	},
+	{
+		id: 5,
+		name: "Product 5",
+		code: "56789012",
+		quantity: 500,
+		rawMaterials: ["56789012", "67890123", "78901234"],
+		batchNo: "56789012",
+	},
+];
+
+const columns: ColumnDef<TMyProduct>[] = [
+	{
+		id: "id",
+		header: ({ table }) => <Checkbox checked={table.getIsAllRowsSelected()} onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)} aria-label="Select all rows" />,
+		cell: ({ row }) => <Checkbox checked={row.getIsSelected()} onCheckedChange={(value) => row.toggleSelected(!!value)} aria-label="Select this row" />,
+		enableSorting: false,
+		enableHiding: false,
+	},
+	{
+		accessorKey: "name",
+		header: "Name",
+		cell: ({ row }) => <div>{row.getValue("name")}</div>,
+	},
+	{
+		accessorKey: "code",
+		header: "Code",
+		cell: ({ row }) => <div>{row.getValue("code")}</div>,
+	},
+	{
+		accessorKey: "quantity",
+		header: "Quantity",
+		cell: ({ row }) => <div>{row.getValue("quantity")}</div>,
+	},
+	{
+		accessorKey: "rawMaterials",
+		header: "Raw Materials",
+		cell: ({ row }) => (
+			<div>
+				{(row.getValue("rawMaterials") as string[])?.map((rawMaterial: string) => {
+					return <div>{rawMaterial}</div>;
+				})}
+			</div>
+		),
+	},
+	{
+		accessorKey: "batchNo",
+		header: "Batch No.",
+		cell: ({ row }) => <div>{row.getValue("batchNo")}</div>,
+	},
+	{
+		id: "actions",
+		enableHiding: false,
+		cell: ({ row: _ }) => (
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant="ghost" className="h-8 w-8 p-0">
+						<span className="sr-only">Open options</span>
+						<MoreHorizontal className="h-4 w-4" />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align="end">
+					<DropdownMenuItem>View</DropdownMenuItem>
+					<DropdownMenuItem>Edit</DropdownMenuItem>
+					<DropdownMenuItem>Delete</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		),
+	},
+];
 
 const MyProducts: NextPageWithLayout = () => {
-	return <div>MyProducts</div>;
+	const [sorting, setSorting] = useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+	const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+	const [rowSelection, setRowSelection] = useState<Record<string, boolean>>({});
+
+	const table = useReactTable({
+		data: myProducts,
+		columns,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		onColumnVisibilityChange: setColumnVisibility,
+		onRowSelectionChange: setRowSelection,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+			rowSelection,
+		},
+	});
+	return (
+		<div className="w-full px-4">
+			<div className="flex item-center py-4">
+				<Input
+					placeholder="Filter products..."
+					value={(table.getColumn("code")?.getFilterValue() as string) ?? ""}
+					onChange={(event) => table.getColumn("code")?.setFilterValue(event.target.value)}
+					className="max-w-sm"
+				/>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="outline" className="ml-auto">
+							Columns <ChevronDown className="ml-2 h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						{table
+							.getAllColumns()
+							.filter((column) => column.getCanHide())
+							.map((column) => {
+								return (
+									<DropdownMenuCheckboxItem key={column.id} className="capitalize" checked={column.getIsVisible()} onCheckedChange={(value) => column.toggleVisibility(!!value)}>
+										{column.id}
+									</DropdownMenuCheckboxItem>
+								);
+							})}
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+			<div className="rounded-md border">
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => (
+									<TableHead key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+								))}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell colSpan={columns.length} className="h-24 text-center">
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<div className="flex items-center justify-end space-x-2 py-4">
+				<div className="flex-1 text-sm text-muted-foreground">
+					{table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
+				</div>
+				<div className="space-x-2">
+					<Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+						Previous
+					</Button>
+					<Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+						Next
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 MyProducts.getLayout = function getLayout(page) {
